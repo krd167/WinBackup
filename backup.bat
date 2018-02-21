650913975e6060c259158649f3897d81ffc11a61
krd167 <36714147+krd167@users.noreply.github.com>
2018-02-21 17:31:24 -0500

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,5 +1,5 @@
 If Not Exist "D:\Users" mkdir "D:\Users"
 
-C:\Windows\system32\robocopy.exe "C:\Users\Kevin" "D:\Users\Kevin" /s /r:1 /w:1 /mt:32
+C:\Windows\system32\robocopy.exe "C:\Users\{EnterProfileNameHere}" "D:\Users\{EnterProfileNameHere}" /s /r:1 /w:1 /mt:32
 
 pause
